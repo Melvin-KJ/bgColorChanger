91b7a0974983606cc6e8fa7bdcc19f5180142490
Melvin-KJ <melvinofficial2001@gmail.com>
2024-10-16 16:13:10 +0530

    Add useState to manage background color state and change background color on button click

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,42 @@
 import { useState } from 'react';
 
 function App() {
-  const [color, setColor] = useState('olive');
+  const [color, setColor] = useState('burlywood');
   return (
     <div
       className="w-full h-screen duration-200"
       style={{ backgroundColor: color }}
     >
-      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-        <div className='flex flex-wrap justify-center gap-3 shadow-xl bg-white px-3 py-2 rounded-3xl '>
-          <button>Red</button>
-          <button>Blue</button>
-          <button>Green</button>
-          <button>Purple</button>
+      <div className="fixed flex flex-wrap justify-center bottom-11 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3  px-4 py-2">
+          <button
+            onClick={() => setColor('red')}
+            className="outline-none px-8 py-2 rounded-full text-white border-white border-2"
+            style={{ backgroundColor: 'red' }}
+          >
+            Red
+          </button>
+          <button
+            onClick={() => setColor('blue')}
+            className="outline-none px-8 py-2 rounded-full text-white  border-white border-2"
+            style={{ backgroundColor: 'blue' }}
+          >
+            Blue
+          </button>
+          <button
+            onClick={() => setColor('green')}
+            className="outline-none px-8 py-2 rounded-full text-white border-white border-2"
+            style={{ backgroundColor: 'green' }}
+          >
+            Green
+          </button>
+          <button
+            onClick={() => setColor('purple')}
+            className="outline-none px-8 py-2 rounded-full text-white border-white border-2"
+            style={{ backgroundColor: 'purple' }}
+          >
+            Purple
+          </button>
         </div>
       </div>
     </div>
